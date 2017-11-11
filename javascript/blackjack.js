6237c8534b3c86fdeca3ecc3393097164f6ad02b
jsimpson15011 <joseph.franklin.simpson@gmail.com>
2017-11-10 22:58:12 -0700

    Added Contact button
    created icon and added svg

--- javascript/blackjack.js
+++ javascript/blackjack.js
@@ -1,4 +1,5 @@
 var test= document.getElementById('test');
+var mainHandActive= true;
 var playerHasSplit= false; 
 var decks=1; //number of decks in game
 var shoeDone=0; //when this number reaches 48 a new shoe starts
@@ -10,6 +11,7 @@ var dealButton= document.getElementById('deal');
 var doubleDownButton= document.getElementById('double-down');
 var table = document.getElementById('table');
 var splitButtonOn= false;
+var splitModeOn= false;
 var cardValue;
 var playerBlackJack = false;
 var hasHighAce= false;
@@ -32,6 +34,19 @@ var h9 = {value: 9, suit:'heart',ace:false, index:8, drawn:0};
 var hj = {value: 10, suit:'heart',ace:false, index:9, drawn:0};
 var hq = {value: 10, suit:'heart',ace:false, index:10, drawn:0};
 var hk = {value: 10, suit:'heart',ace:false, index:11, drawn:0};
+
+for (var i = 1; i < 101; i++) {
+  if(i%3==0) {
+    document.write("fizz");
+  }
+  if(i%5==0) {
+    document.write("buzz");
+  }
+  else if (i%3!=0) {
+  document.write(i);
+  }
+  document.write("</br>");
+}
 
 hitButton.addEventListener('click',hit,false);
 dealButton.addEventListener('click',deal,false);
@@ -179,6 +194,47 @@ function blackJack(){
   dealButton.style.display = 'block';
 }
 
+function dealDebug(){
+  var dealerTotalDisplay =document.getElementById('dealer-total');
+  var playerTotalDisplayLeft =document.getElementById('player-total-left');
+  var firstCardValue;
+  function displayButtons(){
+    doubleDownButton.style.display='block';
+    hitButton.style.display = 'block';
+    standButton.style.display='block';
+  }
+  function setFirstCardValue(){
+    firstCardValue=cardValue;
+    test.innerHTML = firstCardValue;
+  }  
+  function splitBlackJackCheck(){
+    if (playerTotal/2==firstCardValue||(firstCardValue=='A'&&playerTotal==12)) {
+      splitButtonOn= true;
+      splitButton.style.display='block';
+    }
+    if (playerTotal==21) {
+      blackJack();
+    }
+  }
+  playerTotal=0;
+  dealerTotal=0;
+  $('.cards-player').remove();
+  playerTotalDisplayLeft.innerHTML= 'Total: 0';
+  dealerTotalDisplay.innerHTML= '';
+  gameTextLeft.style.display='none';
+  hasAce=false;
+  hasHighAce=false;
+  dealerHasHighAce=false;
+  window.setTimeout(dealerHitFaceDown,1);
+  window.setTimeout(dealerHit,2); 
+  window.setTimeout(hit,3);
+  window.setTimeout(setFirstCardValue,4);
+  window.setTimeout(hit,5);
+  window.setTimeout(splitBlackJackCheck,7);
+  window.setTimeout(displayButtons,6);
+  dealButton.style.display = 'none';
+}
+
 function deal(){
   var dealerTotalDisplay =document.getElementById('dealer-total');
   var playerTotalDisplayLeft =document.getElementById('player-total-left');
@@ -216,22 +272,27 @@ function deal(){
   window.setTimeout(setFirstCardValue,2001);
   window.setTimeout(hit,2003);
   window.setTimeout(splitBlackJackCheck,2004);
-  window.setTimeout(displayButtons,3000);
+  window.setTimeout(displayButtons,2003);
   dealButton.style.display = 'none';
 }
 
 function bust(){
-  var faceDownCard =document.getElementsByClassName('cards-player-face-down')[0];
-  var dealerTotalDisplay =document.getElementById('dealer-total');
-  gameTextLeft.style.display='block';
-  standButton.style.display = 'none';
-  hitButton.style.display = 'none';
-  dealButton.style.display = 'block';
-  faceDownCard.classList.add('cards-player');
-  faceDownCard.innerHTML = faceDownValue;
-  faceDownCard.classList.remove('cards-player-face-down');
-  dealerTotal+= faceDownValue;
-  dealerTotalDisplay.innerHTML = "Total: "+dealerTotal;
+  if (splitModeOn==false) {
+    var faceDownCard =document.getElementsByClassName('cards-player-face-down')[0];
+    var dealerTotalDisplay =document.getElementById('dealer-total');
+    gameTextLeft.style.display='block';
+    standButton.style.display = 'none';
+    hitButton.style.display = 'none';
+    dealButton.style.display = 'block';
+    faceDownCard.classList.add('cards-player');
+    faceDownCard.innerHTML = faceDownValue;
+    faceDownCard.classList.remove('cards-player-face-down');
+    dealerTotal+= faceDownValue;
+    dealerTotalDisplay.innerHTML = "Total: "+dealerTotal;
+  }
+  else{
+    changeHands();
+  }
 }
 
 function hit(){	
@@ -246,67 +307,68 @@ function hit(){
 	if (playerTotal>21) { //if player is busted new hand.
 		bust();
 	}
-
-	newCardValue();
-	while (cardValue=='redraw') {
-		newCardValue();
-		if (shoeDone>=(48*decks)) {
-			h1.drawn=0;
-			h2.drawn=0;
-			h3.drawn=0;
-			h4.drawn=0;
-			h5.drawn=0;
-			h6.drawn=0;
-			h7.drawn=0;
-			h8.drawn=0;
-			h9.drawn=0;
-			hj.drawn=0;
-			hq.drawn=0;
-			hk.drawn=0;
-			shoeDone=0;
-		}
-	}
-	if (cardValue==11&&hasAce==true) {
-			cardValue=1;
-	}
-	if (cardValue==11) {
-		if (playerTotal<=10) {
-			hasHighAce=true;
-			}
-		if (playerTotal>10) {
-			cardValue=1;
-		}
-			hasAce=true;
-	}
-	playerTotal+= cardValue;
-
-	if (hasHighAce==true&&playerTotal>21) {
-		playerTotal-=10;
-		hasHighAce=false;
-		if (cardValue==11) {
-			hasHighAce=true;
-		}
-	}
-	
-	newCard.classList.add('cards-player');
-	if (cardValue==11||cardValue==1) {
-		cardValue='A';
-	}
-	newCard.innerHTML = cardValue;
-	if (hasHighAce==false||playerTotal==21) {
-		playerTotalDisplayLeft.innerHTML = "Total: "+playerTotal;
-	}
-	if (hasHighAce==true&&playerTotal!=21) {
-		playerTotalDisplayLeft.innerHTML = "Total: "+playerTotal+' or '+(playerTotal-10);
-	}
-	left.appendChild(newCard);
-  $(newCard).hide().delay(200).show("slow");
-	if (playerTotal>21) {
-		bust();
-	}
-	if (playerTotal==21&&playerBlackJack!=true) {
-		stand();
-	}
+  if(mainHandActive==true) {
+  	newCardValue();
+  	while (cardValue=='redraw') {
+  		newCardValue();
+  		if (shoeDone>=(48*decks)) {
+  			h1.drawn=0;
+  			h2.drawn=0;
+  			h3.drawn=0;
+  			h4.drawn=0;
+  			h5.drawn=0;
+  			h6.drawn=0;
+  			h7.drawn=0;
+  			h8.drawn=0;
+  			h9.drawn=0;
+  			hj.drawn=0;
+  			hq.drawn=0;
+  			hk.drawn=0;
+  			shoeDone=0;
+  		}
+  	}
+  	if (cardValue==11&&hasAce==true) {
+  			cardValue=1;
+  	}
+  	if (cardValue==11) {
+  		if (playerTotal<=10) {
+  			hasHighAce=true;
+  			}
+  		if (playerTotal>10) {
+  			cardValue=1;
+  		}
+  			hasAce=true;
+  	}
+  	playerTotal+= cardValue;
+  
+  	if (hasHighAce==true&&playerTotal>21) {
+  		playerTotal-=10;
+  		hasHighAce=false;
+  		if (cardValue==11) {
+  			hasHighAce=true;
+  		}
+  	}
+  	
+  	newCard.classList.add('cards-player');
+  	if (cardValue==11||cardValue==1) {
+  		cardValue='A';
+  	}
+  	newCard.innerHTML = cardValue;
+  	if (hasHighAce==false||playerTotal==21) {
+  		playerTotalDisplayLeft.innerHTML = "Total: "+playerTotal;
+  	}
+  	if (hasHighAce==true&&playerTotal!=21) {
+  		playerTotalDisplayLeft.innerHTML = "Total: "+playerTotal+' or '+(playerTotal-10);
+  	}
+  	left.appendChild(newCard);
+    $(newCard).hide().delay(200).show("slow");
+  	if (playerTotal>21) {
+  		bust();
+  	}
+  	if (playerTotal==21&&playerBlackJack!=true) {
+  		stand();
+  	}
+  }
 }
 
 function dealerHit(){
@@ -393,31 +455,56 @@ function dealerHitFaceDown(){
 }
 
 function stand(){
-  var faceDownCard =document.getElementsByClassName('cards-player-face-down')[0];
-  var dealerTotalDisplay = document.getElementById("dealer-total");
-  standButton.style.display='none';
-  hitButton.style.display= 'none';
-  splitButton.style.display= 'none';
-  dealButton.style.display= 'block';
-  doubleDownButton.style.display='none';
-  faceDownCard.classList.add('cards-player');
-  faceDownCard.innerHTML = faceDownValue;
-  faceDownCard.classList.remove('cards-player-face-down');
-  dealerTotal+= faceDownValue;
-  while (dealerTotal<17) {
-    dealerHit();
-  }
+  if (splitModeOn==false) {
+    var faceDownCard =document.getElementsByClassName('cards-player-face-down')[0];
+    var dealerTotalDisplay = document.getElementById("dealer-total");
+    standButton.style.display='none';
+    hitButton.style.display= 'none';
+    splitButton.style.display= 'none';
+    dealButton.style.display= 'block';
+    doubleDownButton.style.display='none';
+    faceDownCard.classList.add('cards-player');
+    faceDownCard.innerHTML = faceDownValue;
+    faceDownCard.classList.remove('cards-player-face-down');
+    dealerTotal+= faceDownValue;
+    while (dealerTotal<17) {
+      dealerHit();
+    }
   dealerTotalDisplay.innerHTML = "Total: "+dealerTotal;
+  }
+  else{
+    changeHands();
+  }
 }
 
 function split(){
+  var left = document.getElementById('left');
   var playerTotalDisplayRight= document.getElementById('player-total-right');
+  var playerTotalDisplayLeft= document.getElementById('player-total-left');
   var newCard = document.createElement("div");
   var right = document.getElementById('right');
+  $('#left div:nth-child(1)').remove();
+  if (playerTotal==13) {
+    playerTotal=11;
+  }
+  else{
+    playerTotal=(playerTotal/2);
+  }
+  playerTotalDisplayLeft.innerHTML = "Total: "+playerTotal;
+  splitModeOn = true;
   newCard.innerHTML = cardValue;
   splitButton.style.display='none';
   newCard.classList.add('cards-player');
   right.appendChild(newCard);
+}
+
+function changeHands(){
+  var right = document.getElementById('right');
+  var left = document.getElementById('left');
+  splitModeOn=false;
+  mainHandActive=false;
+  right.classList.add('active');
+  left.classList.remove('active');
 }
 
 function doubleDown(){
