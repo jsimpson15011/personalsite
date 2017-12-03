30dcfaf589662e082c206c1c7daf1d9b279c558d
jsimpson15011 <joseph.franklin.simpson@gmail.com>
2017-12-03 10:24:16 -0700

    working on show more info on home

--- javascript/homeinfoboxes.js
+++ javascript/homeinfoboxes.js
@@ -1,13 +1,15 @@
+(function() {
 var bubbles= document.getElementsByClassName('home-animation-container');
 var moreInfo= document.getElementsByClassName('more-info-box');
+var greyOut= document.getElementById('grey-out');
+var bubbleIndex;
 
 for (var i = 0; i < bubbles.length; i++) {
 	bubbles[i].addEventListener('click',showMoreInfo,false);
 }
-
+greyOut.addEventListener('click',hideMoreInfo,false);
 function showMoreInfo() {
 	var rawBubbleIndex = 0;
-	var bubbleIndex;
 	var child = this;
 	while( (child = child.previousSibling) != null ) 
   	rawBubbleIndex++;
@@ -22,8 +24,11 @@ function showMoreInfo() {
 }
 
 function hideMoreInfo() {
+	moreInfo[bubbleIndex].classList.add('hidden');
 	$('html, body').css({//returns scrolling
     overflow: 'auto',
     height: 'auto'
 	});
 }
+
+})()
